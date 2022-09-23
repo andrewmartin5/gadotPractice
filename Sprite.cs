9d61f45ddc7a3790796061857153cf7a6e8b23e7
andrewmartin5 <andrewmlefty5@gmail.com>
2022-09-23 19:19:33 -0400

    Sure did something here

--- Sprite.cs
+++ Sprite.cs
@@ -13,14 +13,28 @@ public class Sprite : Godot.Sprite
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-
+		Timer timer = GetNode<Timer>("Timer");
+		timer.Connect("timeout", this, "_on_Timer_timeout");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(float delta)
 	{
-		var velocity = Vector2.Up.Rotated(Rotation) * Speed;
 		Rotation += AngularSpeed * delta;
+		var velocity = Vector2.Up.Rotated(Rotation) * Speed;
+
 		Position += velocity * delta;
 	}
+	
+	private void _on_Button_pressed()
+	{
+		SetProcess(!IsProcessing());
+	}
+
+	private void _on_Timer_timeout(){
+		Visible = !Visible;
+	}
 }
+
+
+
